3948e0fc3d83c22afa64784f638c1828c1e0ac74
mglagola <mark.glagola@gmail.com>
2017-04-18 09:00:16 -0500

    initial

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const Promise = require('bluebird');
+global.Promise = Promise;
+
+const program = require('commander');
+const _exec = require("child_process").exec;
+const fs = Promise.promisifyAll(require("fs"));
+
+function exec (cmd) {
+    return new Promise((resolve, reject) => {
+        _exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                return reject(error);
+            } else if (stderr) {
+                return reject(stderr);
+            } else {
+                return resolve(stdout);
+            }
+        });
+    });
+}
+
+program.version('1.0');
+
+program
+    .command('publish <message>')
+    .action(async (message) => {
+        if (!message) {
+            console.error('No git tag message supplied, see help');
+            return;
+        }
+
+        try {
+            const data = await fs.readFileAsync('package.json', 'utf8');
+            const json = JSON.parse(data);
+            const version = json.version;
+            await exec('ls');
+            await exec(`git tag -a v${version} -m "${message}"`);
+            process.exit(0);
+        } catch (error) {
+            throw error;
+        }
+    });
+
+program.parse(process.argv);
