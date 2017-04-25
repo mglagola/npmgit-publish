2cd4a0d04dc6d3cf8510644e6e940d0f64b13f13
mglagola <mark.glagola@gmail.com>
2017-04-25 15:06:07 -0500

    chalk + handling dirty git status

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ global.Promise = Promise;
 const program = require('commander');
 const _exec = require("child_process").exec;
 const fs = Promise.promisifyAll(require("fs"));
+const chalk = require('chalk');
 
 function exec (cmd, ignoreStterr = false) {
     return new Promise((resolve, reject) => {
@@ -23,6 +24,9 @@ function exec (cmd, ignoreStterr = false) {
     });
 }
 
+const logGreen = (message) => console.log(chalk.green(message));
+const logRed = (message) => console.log(chalk.red(message));
+
 program.version('1.0');
 
 program
@@ -34,18 +38,24 @@ program
         }
 
         try {
+            const isDirty = (await exec('git status -s')).length > 0;
+            if (isDirty) {
+                throw new Error('Seems like you have uncommitted changes locally, aborting publish');
+            }
+
             const data = await fs.readFileAsync('package.json', 'utf8');
             const json = JSON.parse(data);
             const version = json.version;
-            console.log(`Published npm version: ${version}`);
+            logGreen(`Published npm version: ${version}`);
             await exec('npm publish');
             await exec(`git tag -a v${version} -m "${message}"`);
-            console.log(`Git tagged version: ${version} with message: ${message}`);
+            logGreen(`Git tagged version: ${version} with message: ${message}`);
             await exec(`git push --tags`, true);
-            console.log(`Pushed tagged version, ${version}, to git repo`);
+            logGreen(`Pushed tagged version, ${version}, to git repo`);
             process.exit(0);
         } catch (error) {
-            throw error;
+            logRed(error.message);
+            process.exit(1);
         }
     });
 
